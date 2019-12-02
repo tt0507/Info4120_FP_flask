daab1a31597151fa1795b98bcc04d313a7846544
Toshi Tokuyama <tt426@cornell.edu>
2019-12-02 16:50:00 -0500

    update send_survey.py

--- app/send_survey.py
+++ app/send_survey.py
@@ -1,7 +1,5 @@
 from twilio.rest import Client
 
-account_sid = 'AC9ecc9fc2e6eb35838aeeb9cf89e203cd'
-auth_token = '23823aa2c28b82d3994f997fcea4adb8'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
