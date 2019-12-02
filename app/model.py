030ee8e4f06a5321d1b9cc8df6fe176db0a1c390
Toshi Tokuyama <tt426@cornell.edu>
2019-12-02 16:10:43 -0500

    minor edits - formatting, home.html, index.html

--- app/model.py
+++ app/model.py
@@ -5,42 +5,42 @@ from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
-	return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(20), unique=True, nullable=False)
-	password = db.Column(db.String(60), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
-	def __repr__(self):
-		return f"User('{self.id}','{self.username}', '{self.password}')"
+    def __repr__(self):
+        return f"User('{self.id}','{self.username}', '{self.password}')"
 
 
 class Answer(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	user = db.relationship('User', backref='user', lazy=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	log = db.relationship('Log', backref='log', lazy=True)
-	log_number = db.Column(db.Integer, db.ForeignKey('log.id'), nullable=False)
-	question1 = db.Column(db.String(20), unique=True, nullable=False)
-	question2 = db.Column(db.String(20), unique=True, nullable=False)
-	question3 = db.Column(db.String(20), unique=True, nullable=False)
-	question4 = db.Column(db.String(20), unique=True, nullable=False)
-	question5 = db.Column(db.String(20), unique=True, nullable=False)
-	date_answer = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.relationship('User', backref='user', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    log = db.relationship('Log', backref='log', lazy=True)
+    log_number = db.Column(db.Integer, db.ForeignKey('log.id'), nullable=False)
+    question1 = db.Column(db.String(20), unique=True, nullable=False)
+    question2 = db.Column(db.String(20), unique=True, nullable=False)
+    question3 = db.Column(db.String(20), unique=True, nullable=False)
+    question4 = db.Column(db.String(20), unique=True, nullable=False)
+    question5 = db.Column(db.String(20), unique=True, nullable=False)
+    date_answer = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-	def __repr__(self, ):
-		return f"User('{self.id}','{self.user_id}', '{self.log_numberd}', '{self.question1}', '{self.question2}', " \
-		       f"'{self.question3}', '{self.question4}', '{self.question5}', '{self.date_answer}')"
+    def __repr__(self, ):
+        return f"User('{self.id}','{self.user_id}', '{self.log_numberd}', '{self.question1}', '{self.question2}', " \
+               f"'{self.question3}', '{self.question4}', '{self.question5}', '{self.date_answer}')"
 
 
 class Log(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	user = db.relationship('User', backref='user_id', lazy=True)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	log_number = db.Column(db.Integer)
-	recent_sent_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.relationship('User', backref='user_id', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    log_number = db.Column(db.Integer)
+    recent_sent_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-	def __repr__(self):
-		return f"User('{self.id}','{self.user_id}', '{self.log_numberd}', '{self.recent_sent_date}')"
+    def __repr__(self):
+        return f"User('{self.id}','{self.user_id}', '{self.log_numberd}', '{self.recent_sent_date}')"
--- app/routes.py
+++ app/routes.py
@@ -7,69 +7,68 @@ from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
 def index():
-	return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home')
 
 
 @app.route('/about')
 def about():
-	return render_template('about.html', title='About')
+    return render_template('about.html', title='About')
 
 
 @app.route('/home')
 def home():
-	return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-	form = LoginForm()
-	if current_user.is_authenticated:
-		return redirect(url_for('home'))
-	if form.validate_on_submit():
-		user = User.query.filter_by(username=form.username.data).first()
-		if user and (user.password == form.password.data):
-			login_user(user, remember=form.remember.data)
-			next_page = request.args.get('next')
-			return redirect(next) if next_page else redirect(url_for('home'))
-		else:
-			flash('Login unsuccessful. Please check username and password', 'danger')
-	return render_template('login.html', title='Login', form=form)
+    form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and (user.password == form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 @app.route('/logout')
 def logout():
-	logout_user()
-	return redirect(url_for('index'))
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/log')
 @login_required
 def log():
-	return render_template('log.html', title='Log')
+    return render_template('log.html', title='Log')
 
 
 @app.route('/survey', methods=('POST', 'GET'))
 def survey():
-	form = Survey()
-	# answer1 = form.question1.data
-	if request.method == 'POST':
-		answer1 = form.question1.data
-		answer2 = form.question2.data
-		answer3 = form.question3.data
-		answer4 = form.question4.data
-		return redirect(url_for('record', answer1=answer1, answer2=answer2, answer3=answer3, answer4=answer4))
-	if not form.validate_on_submit():
-		print('Form not validating')
-	return render_template('survey.html', title='Survey', form=form)
+    form = Survey()
+    # answer1 = form.question1.data
+    if request.method == 'POST':
+        answer1 = form.question1.data
+        answer2 = form.question2.data
+        answer3 = form.question3.data
+        answer4 = form.question4.data
+        return redirect(url_for('record', answer1=answer1, answer2=answer2, answer3=answer3, answer4=answer4))
+    if not form.validate_on_submit():
+        print('Form not validating')
+    return render_template('survey.html', title='Survey', form=form)
 
 
 @app.route('/record')
 def record():
-	answer1 = request.args.get('answer1')
-	answer2 = request.args.get('answer2')
-	answer3 = request.args.get('answer3')
-	answer4 = request.args.get('answer4')
+    answer1 = request.args.get('answer1')
+    answer2 = request.args.get('answer2')
+    answer3 = request.args.get('answer3')
+    answer4 = request.args.get('answer4')
 
-	data = Answer()
-	return render_template('record.html', title='Record', answer1=answer1, answer2=answer2, answer3=answer3,
-	                       answer4=answer4)
+    data = Answer()
+    return render_template('record.html', title='Record')
