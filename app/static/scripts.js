9b3c2b156bd4e28bbe91acf5844e2889a0f186ee
Toshi Tokuyama <tt426@cornell.edu>
2019-12-01 22:43:18 -0500

    fitbit api

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,1 +1,18 @@
-console.log('hello');
+// OAuth2 Authorization
+var url = window.location.href;
+var access_token = url.split("#")[1].split("=")[1].split("&")[0];
+var userId = url.split("#")[1].split("=")[2].split("&")[0];
+
+console.log(access_token);
+console.log(userId);
+
+// Get data from fitbit
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.fitbit.com/1/user/' + userId + '/activities/heart/date/today/1w.json');
+xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        console.log(xhr.responseText)
+    }
+};
+xhr.send();
